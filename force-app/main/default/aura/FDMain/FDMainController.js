c57b180f0563d5129bde7d877a2ec5a5161123fc
Alexey Sachevichik <roim@ya.ru>
2019-07-09 17:55:36 +0300

    Create authorization on app

--- force-app/main/default/aura/FDMain/FDMainController.js
+++ force-app/main/default/aura/FDMain/FDMainController.js
@@ -1,9 +1,15 @@
 ({
     init: function(component, event, helper) {
-        helper.getCode(component);
+        helper.getCredential(component, event);
+        helper.listenAuthorizationCode(component, event);
     },
 
-    handleClickConnectToBox: function(component, event, helper) {
-        helper.firstConnection(component, event);
-    },
+    // handleClickConnectToBox: function(component, event, helper) {
+    //     helper.firstConnection(component, event);
+    // },
+
+    // handleClickgetRootInformation: function(component, event, helper) {
+        // helper.getFoldersInfo(component, '0');
+        // helper.getFoldersInfo(component, '81389432619');
+    // },
 })
--- force-app/main/default/aura/FDMain/FDMainHelper.js
+++ force-app/main/default/aura/FDMain/FDMainHelper.js
@@ -1,54 +1,107 @@
 ({
-    // handleClickConnectToBox: function(component, event, helper) {
-
-    //     var url = 'https://account.box.com/api/oauth2/authorize';
-    //     url += '?response_type=code';
-
-    //     if (component.get('v.CLIENT_ID').length > 0) {
-    //         url += '&client_id=' + component.get('v.CLIENT_ID');
-    //     }
-    //     if (component.get('v.REDIRECT_URI').length > 0) {
-    //         url += '&redirect_uri=' + component.get('v.REDIRECT_URI');
-    //     }
-    //     if (component.get('v.CLIENT_SECRET').length > 0) {
-    //         url += '&state=security_token%' + component.get('v.CLIENT_SECRET');
-    //     }
-    //     if (component.get('v.LOGIN').length > 0) {
-    //         url += '&box_login=' + component.get('v.LOGIN');
-    //     }
-
-    //     // console.log(url);
-
-    //     var urlEvent = $A.get("e.force:navigateToURL");
-    //     urlEvent.setParams({'url': url});
-    //     urlEvent.fire();
-    // },
-
-    firstConnection: function(component, event) {
-        var action = component.get('c.firstConnect');
+    getCredential: function(component, event) {
+        var action = component.get('c.getCredential');
 		action.setCallback(this, function(response) {
 			if (response.getState() === 'SUCCESS') {
-                var urlEvent = $A.get("e.force:navigateToURL");
-                urlEvent.setParams({'url': response.getReturnValue()});
-                urlEvent.fire();
+                if (JSON.stringify({}) === JSON.stringify(response.getReturnValue())) {
+                    this.eventNavigateToAuthorizationPage(component);
+                } else {
+                    this.checkToken(component);
+                }
             }
         });
 		$A.enqueueAction(action);
     },
 
-    getCode: function(component, event) {
-        var authorizationCode = decodeURIComponent(window.location.search.substring(6));
-        if (authorizationCode.length > 0) {
-            var action = component.get('c.getToken');
-            action.setParams({'authorizationCode': authorizationCode});
+    eventNavigateToHomePage: function(component) {
+        var action = component.get('c.getRedirectURI');
+		action.setCallback(this, function(response) {
+            if (response.getState() === 'SUCCESS') {
+                this.eventNavigateTo(response.getReturnValue());
+            }
+        });
+		$A.enqueueAction(action);
+    },
+
+    eventNavigateToAuthorizationPage: function(component) {
+        var action = component.get('c.getAuthorizationCodeLink');
+		action.setCallback(this, function(response) {
+            if (response.getState() === 'SUCCESS') {
+                this.eventNavigateTo(response.getReturnValue());
+            }
+        });
+		$A.enqueueAction(action);
+    },
+
+    eventNavigateTo: function(url) {
+        window.location.replace(url);
+    },
+    
+    listenAuthorizationCode: function(component) {
+        var search = decodeURIComponent(window.location.search);
+        var authorizationCode = search.substring(6);
+        if (
+            search.substring(0,1) === '?' &&
+            search.substring(1,5) === 'code' &&
+            search.substring(5,6) === '=' &&
+            authorizationCode.length > 0
+        ) {
+            this.getToken(component, authorizationCode);
+        }
+    },
+        
+    getToken: function(component, authorizationCode) {
+        var action = component.get('c.getToken');
+        action.setParams({authorizationCode});
+        action.setCallback(this, function(response) {
+            if (response.getState() === 'SUCCESS') {
+                this.checkStatusToken(component, response.getReturnValue(), authorizationCode);
+            }
+        });
+        $A.enqueueAction(action);
+    },
+    
+    checkStatusToken: function(component, response, authorizationCode) {
+        var result = JSON.parse(response);
+        if (result.access_token && result.expires_in && result.refresh_token && result.token_type) {
+            var action = component.get('c.saveCredential');
+            action.setParams({
+                authorizationCode,
+                'accessToken': result.access_token,
+                'expiresIn': result.expires_in,
+                'refreshToken': result.refresh_token,
+                'tokenType': result.token_type.substring(0,1).toUpperCase() + result.token_type.substring(1).toLowerCase()
+            });
             action.setCallback(this, function(response) {
                 if (response.getState() === 'SUCCESS') {
-                    var result = JSON.parse(response.getReturnValue());
-                    console.log(result);
-                    // console.log(response.getReturnValue());
+                    this.eventNavigateToHomePage(component);
                 }
             });
             $A.enqueueAction(action);
+        } else if (result.error && result.error_description) {
+            this.eventNavigateToAuthorizationPage(component);
         }
     },
+
+    checkToken: function(component) {
+        component.set('v.isLoadingCredential', false);
+        console.log('credential exist, check it');
+        // обновим токен
+    },
+
+    // getFoldersInfo: function(component, folderId) {
+    //     var action = component.get('c.getFoldersInfo');
+    //     action.setParams({folderId});
+    //     action.setCallback(this, function(response) {
+    //         if (response.getState() === 'SUCCESS') {
+    //             var result = JSON.parse(response.getReturnValue());
+    //             console.log(result);
+
+    //             if (result.item_collection && result.item_collection.entries && result.item_collection.entries.length > 0) {
+    //                 component.set('v.entries', result.item_collection.entries);
+    //             }
+    //         }
+    //     });
+    //     $A.enqueueAction(action);
+    // },
 })
